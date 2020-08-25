3afba6ef30c80a294b84c841b9e28dd2cf3630bc
spm <majining@users.noreply.github.com>
2020-08-24 21:12:59 -0500

    mongodb webapi tutorial, through the step "test the web api"

---
+++ BooksAPI/BooksAPI/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BooksAPI.Models
+{
+    public class Book
+    {
+        [BsonId] // Designates this property as the document's primary key
+        [BsonRepresentation(BsonType.ObjectId)] // allows passing the parameter as type string instead of ObjectId structure. MongoDB handles the conversion to ObjectId.
+        public string Id { get; set; }
+
+        [BsonElement("Name")] // associates this property with the Name property in the DB collection.
+        public string BookName { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+    }
+}
